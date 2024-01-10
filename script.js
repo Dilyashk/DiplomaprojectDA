d336663f2f39aaf71b6e4b397d4911e5405d9f8f
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-01-10 21:59:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+
+
+import React from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+
+const RoleSelection = ({ onSelectRole }) => {
+  return (
+    <View style={styles.container}>
+      <Button title="Volunteer" onPress={() => onSelectRole('volunteer')} />
+      <Button title="User" onPress={() => onSelectRole('user')} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default RoleSelection;
