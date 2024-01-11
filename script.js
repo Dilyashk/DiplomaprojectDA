d15098d4112ad72d3abf98a080aafc8d9105b6a6
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-01-11 19:52:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -61,19 +61,5 @@ export default RoleSelection;
 
 
 
-//delete 2
-function resetAllMenus() {
-  setTimeout(function () {
-    var x = document.getElementsByClassName("multiSelect");
-    var i;
-    for (i = 1; i < x.length; i++) {
-      x[i].style.transform = "translateX(100%)";
-      x[i].style.clipPath = "polygon(0 0, 0 0, 0 100%, 0% 100%)";
-    }
-    document.querySelectorAll(".multiSelect")[0].style.transform =
-      "translateX(0)";
-    document.querySelectorAll(".multiSelect")[0].style.clipPath =
-      "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
-  }, 300);
-}
+
 
