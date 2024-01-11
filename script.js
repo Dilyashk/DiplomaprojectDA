5a1e23560ef9d1f280e0bce92b662a1d3e77e480
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-01-11 19:52:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -35,29 +35,7 @@ const styles = StyleSheet.create({
 export default RoleSelection;
 
 
-//delete 
-import React from 'react';
-import { View, Button, StyleSheet } from 'react-native';
 
-const RoleSelection = ({ onSelectRole }) => {
-  return (
-    <View style={styles.container}>
-      <Button title="Volunteer" onPress={() => onSelectRole('volunteer')} />
-      <Button title="User" onPress={() => onSelectRole('user')} />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
-
-
-export default RoleSelection;
 
 
 
