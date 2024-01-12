a7de1afb3d42f2b2f91447d04bf975a3ca9386b8
Aruzhan Raimkulova <95526098+onigirisin@users.noreply.github.com>
2024-01-12 22:46:20 +0600

    Add files via upload

---
+++ js/App.js
@@ -0,0 +1,36 @@
+// App.js
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RoleSelection from './RoleSelection';
+import LoginScreen from './LoginScreen';
+
+const App = () => {
+  const [selectedRole, setSelectedRole] = useState(null);
+
+  const handleSelectRole = (role) => {
+    setSelectedRole(role);
+  };
+
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        {!selectedRole ? (
+          <RoleSelection onSelectRole={handleSelectRole} />
+        ) : (
+          <LoginScreen role={selectedRole} />
+        )}
+      </View>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ js/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from '@firebase/app';
+import { getAuth } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFxxpE0r3k6tSKXYE12_4F0vzIu-vkJLo',
+  authDomain: 'geka-37d9f.firebaseapp.com',
+  projectId: 'geka-37d9f',
+  storageBucket: 'geka-37d9f.appspot.com',
+  messagingSenderId: '468416351566861363',
+  appId: '1:1025241042379:android:c2688b918faa5e2c4c7a4e',
+};
+
+// Ensure firebaseConfig is defined
+if (!firebaseConfig) {
+  throw new Error('Firebase configuration is missing');
+}
+
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Use AsyncStorage for persistence
+const auth = getAuth(firebaseApp);
+
+export { auth };
