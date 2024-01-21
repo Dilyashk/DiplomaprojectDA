bc94864ed728b2004224bac70729964d3e7ed0de
Aruzhan Raimkulova <95526098+onigirisin@users.noreply.github.com>
2024-01-22 05:05:22 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -55,10 +55,6 @@ function resetAllMenus() {
         showcase.classList.toggle('active');
       })
 
-//del
-const menuToggle = document.querySelector('.toggle');
-      const showcase = document.querySelector('.showcase');
-
       menuToggle.addEventListener('click', () => {
         menuToggle.classList.toggle('active');
         showcase.classList.toggle('active');
