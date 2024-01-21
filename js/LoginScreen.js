98ab6b54cab734f1d0e53fd095faf84178ea576f
Aruzhan Raimkulova <95526098+onigirisin@users.noreply.github.com>
2024-01-22 05:04:05 +0600

    Update LoginScreen.js

--- js/LoginScreen.js
+++ js/LoginScreen.js
@@ -75,4 +75,10 @@ const validateEmail = (email) => {
   return emailRegex.test(email);
 };
 
+
+//const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
 export default LoginScreen;
