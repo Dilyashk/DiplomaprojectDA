ee05d92616163d6a313ddf85508414f3ea3465b7
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-01-26 23:52:32 +0600

    Create total.js

---
+++ js/total.js
@@ -0,0 +1,22 @@
+function Person() {
+  // В конструкторе Person() `this` указывает на себя.
+  this.age = 0;
+
+  setInterval(function growUp() {
+    // В нестрогом режиме, в функции growUp() `this` указывает
+    // на глобальный объект, который отличается от `this`,
+    // определяемом в конструкторе Person().
+    this.age++;
+  }, 1000);
+}
+
+var p = new Person();
+function Person() {
+  this.age = 0;
+
+  setInterval(() => {
+    this.age++; // `this` указывает на объект Person
+  }, 1000);
+}
+
+var p = new Person();
