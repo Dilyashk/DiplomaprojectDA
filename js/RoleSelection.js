c4435f39c5ffb1117be71da534ded59ba042dc9f
Aruzhan Raimkulova <95526098+onigirisin@users.noreply.github.com>
2024-01-28 19:13:24 +0600

    Update RoleSelection.js

--- js/RoleSelection.js
+++ js/RoleSelection.js
@@ -19,12 +19,4 @@ const styles = StyleSheet.create({
   },
 });
 
-\\
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 export default RoleSelection;
