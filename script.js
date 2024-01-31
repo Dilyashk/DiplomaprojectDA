efa845a96e048f51c756ae319f5c1d1da1665af9
Aruzhan Raimkulova <95526098+onigirisin@users.noreply.github.com>
2024-01-31 09:58:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -32,13 +32,6 @@ const styles = StyleSheet.create({
   },
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default RoleSelection;
 
