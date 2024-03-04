ffe82317085107df019d8e62b9200b4d7ede6fe6
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-03-04 18:54:47 +0600

    Update total.js

--- js/total.js
+++ js/total.js
@@ -197,3 +197,14 @@ const App = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+
+export default App;
+
