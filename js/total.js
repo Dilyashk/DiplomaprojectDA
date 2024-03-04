2cd7e09ee5311c4818c2825e949db49c4eb609bd
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-03-04 18:54:38 +0600

    Update total.js

--- js/total.js
+++ js/total.js
@@ -152,3 +152,48 @@ function updateName() {
   const name = prompt("Enter a new name");
   button.textContent = `Player 1: ${name}`;
 }
+
+
+
+
+// App.js
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RoleSelection from './RoleSelection';
+import LoginScreen from './LoginScreen';
+
+const App = () => {
+  const [selectedRole, setSelectedRole] = useState(null);
+
+  const handleSelectRole = (role) => {
+    setSelectedRole(role);
+  };
+
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        {!selectedRole ? (
+          <RoleSelection onSelectRole={handleSelectRole} />
+        ) : (
+          <LoginScreen role={selectedRole} />
+        )}
+      </View>
+    </NavigationContainer>
+  );
+};
+
+
+ return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        {!selectedRole ? (
+          <RoleSelection onSelectRole={handleSelectRole} />
+        ) : (
+          <LoginScreen role={selectedRole} />
+        )}
+      </View>
+    </NavigationContainer>
+  );
+};
+
