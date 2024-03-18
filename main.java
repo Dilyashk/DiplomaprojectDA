e7086b7713e5c0d87d4a115c8521852eff28518f
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-03-18 15:33:17 +0600

    Update main.java

--- main.java
+++ main.java
@@ -30,3 +30,18 @@ public class JavaDownloadFileFromURL {
         }
     }
 
+      private static void downloadUsingStream(String urlStr, String file) throws IOException{
+        URL url = new URL(urlStr);
+        BufferedInputStream bis = new BufferedInputStream(url.openStream());
+        FileOutputStream fis = new FileOutputStream(file);
+        byte[] buffer = new byte[1024];
+        int count=0;
+        while((count = bis.read(buffer,0,1024)) != -1)
+        {
+            fis.write(buffer, 0, count);
+        }
+        fis.close();
+        bis.close();
+    }
+
+
