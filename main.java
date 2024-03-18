f60d9102cf40f37c9b41931da056d9ca7caa230c
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-03-18 15:33:03 +0600

    Update main.java

--- main.java
+++ main.java
@@ -3,3 +3,30 @@ FileUtils.copyURLToFile(
   new File(FILE_NAME), 
   CONNECT_TIMEOUT, 
   READ_TIMEOUT);
+
+
+
+package com.journaldev.files;
+
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class JavaDownloadFileFromURL {
+
+    public static void main(String[] args) {
+        String url = "https://www.journaldev.com/sitemap.xml";
+        
+        try {
+            downloadUsingNIO(url, "/Users/pankaj/sitemap.xml");
+            
+            downloadUsingStream(url, "/Users/pankaj/sitemap_stream.xml");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
