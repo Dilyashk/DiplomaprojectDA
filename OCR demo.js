9eb514a4ea4b2786200dff82c11689a53e76da44
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-05-03 12:36:32 +0500

    Update OCR demo.js

--- OCR demo.js
+++ OCR demo.js
@@ -71,3 +71,34 @@ const getFileFormat = (filename) => {
       reader.onerror = reject;
       reader.readAsDataURL(base64Image);
     });
+
+ const requestData = {
+      mimeType: formatType,
+      languageCodes: ['*'],
+      model: 'page',
+      content: content,
+    };
+
+    console.log('Image data:', requestData);
+
+    try {
+      const response = await fetch('http://192.168.1.10:4000/recognizeText', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Api-Key AQVNzgAdMKmYms98OVk9SexUKLAc3aUXFLjUOKi3',
+          'x-data-logging-enabled': 'true',
+        },
+        body: JSON.stringify(requestData),
+      });
+
+      const data = await response.json();
+      setLoading(false);
+      console.log('Response data:', data);
+      const fullText = data?.result?.textAnnotation?.fullText;
+      setText(fullText || 'No text detected.');
+    } catch (error) {
+      setLoading(false);
+      console.error('Error performing OCR:', error);
+    }
+  };
