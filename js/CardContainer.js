59bafbda4ad7f3b20b7277b992132f52889a6de8
Dilyashk <146911475+Dilyashk@users.noreply.github.com>
2024-05-15 23:25:47 +0500

    Update CardContainer.js

--- js/CardContainer.js
+++ js/CardContainer.js
@@ -16,3 +16,39 @@ function OCRDemo() {
       maxHeight: 500,
       quality: 0.8,
     };
+
+
+    
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        const fileName = response.assets[0].fileName;
+        const uri = response.assets[0].uri;
+        setFilename(fileName);
+        setImage(uri);
+        console.log('Selected image: ', uri);
+      }
+    });
+  };
+
+  const getFileFormat = (filename) => {
+    const parts = filename.split('.');
+    const extension = parts[parts.length - 1].toLowerCase();
+
+    const formatMap = {
+      jpg: 'JPEG',
+      jpeg: 'JPEG',
+      png: 'PNG',
+      gif: 'GIF',
+      bmp: 'BMP',
+      svg: 'SVG',
+      pdf: 'PDF',
+      doc: 'DOC',
+      docx: 'DOCX',
+    };
+
+    return formatMap[extension] || '';
+  };
